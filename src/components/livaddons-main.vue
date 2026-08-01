ecd9a7d0aa61e6c1b6ef439e0bf6a6440327bf49
Livvya <livvya@rape.ink>
2026-08-01 17:49:14 +0200

    layout

--- src/components/livaddons-main.vue
+++ src/components/livaddons-main.vue
@@ -1,9 +1,28 @@
 <script module lang="ts">
-
+import { Icon } from "@iconify/vue"
 </script>
 <template>
-fc
-</template>
-<script>
+    <div class="mainclass">
+        <div class="navbar">
 
-</script>
+        </div>
+        <div class="hero">
+
+        </div>
+        <div class="content">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</template>
+<style scoped>
+.mainclass {
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(17, 17, 17);
+    display: flex;
+    flex-direction: column;
+}
+</style>
